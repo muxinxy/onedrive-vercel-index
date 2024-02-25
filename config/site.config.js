56ea8f4fcc145f70370660c0e7c2bc423b94031f
Xin Mu <zeng5360@gmail.com>
2024-02-26 01:20:05 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/@SynologyCloudSync', '/bt_backup', '/NAS', '/Other'],
+  protectedRoutes: ['/Crypt', '/bt_backup', '/NAS', '/Other', '/Duplicati'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:spencer.wushangbo@gmail.com',
